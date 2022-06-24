f87b80188efe0548264cbba19628a745bef15c77
Haykay2020 <yinkadisa@gmail.com>
2022-06-24 10:13:39 +0100

    zuri js introduction

---
+++ Introduction/task.js
@@ -0,0 +1,9 @@
+// prints my name, height, and ountry on the screen
+
+const data = {
+    name: "ADISA Abdulhakeem",
+    height: "1.71 m",
+    country: "Nigeria",
+  };
+  
+  alert(`Name: ${data.name} \nHeight: ${data.height} \nCountry: ${data.country}`);
